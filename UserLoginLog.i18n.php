b23801c335b9d21658e2780a500060a676b95ac4
Aran Dunkley <aran@organicdesign.co.nz>
2015-04-20 09:41:07 +0000

    re-structure
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2403 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ UserLoginLog.i18n.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Internationalisation file for the UserLoginLog extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'userloginlogpage'     => "User login log",
+	'userloginlogpagetext' => "This is a log of events associated with users logging in or out of the wiki",
+	'userlogin-success'    => "Login completed successfully",
+	'userlogin-error'      => "Login failure from $2",
+	'userlogin-logout'     => "Logout completed successfully",
+	'userloginlogentry'    => "",
+);
